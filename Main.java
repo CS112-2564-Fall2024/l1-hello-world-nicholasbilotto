79d5d55964c2c38d74ce4cad8018aebee48d5110
nick <nickbilotto@gmail.com>
2024-08-21 14:27:25 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello CS 112!");
+        System.out.println("Welcome to CS 112!");
+        String date = "August 21st 2024"
+        int daysRemaining
+        int daysCount = 3
     }
+
+    public static void printDate(date) {
+        System.out.println("Today's Date is " + date);
+    }
+
+    public static daysOfSchoolRemaining(count, remaining) {
+        return remaining - count;
 }
