1e536dd494fb80f44dcb2854f9d3349cc763758b
nick <nickbilotto@gmail.com>
2024-08-21 14:48:36 -0700

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,7 +1,87 @@
 public class Car {
+  //value
+  String make, model, color;
+  int year, mpg, topSpeed;
 
-public static void car{
+  //constructor, default
+  public Car(){
+    this.make = null;
+    this.model = null;
+    this.color = null;
+    this.year = null;
+    this.mpg = null;
+    this.topSpeed = null;
+  }
+
+  //constructor, full
+  public Car(make, model, color, year, mpg, topSpeed){
+    this.make = make;
+    this.model = model;
+    this.color = color;
+    this.year = year;
+    this.mpg = mpg;
+    this.topSpeed = topSpeed;
+  }
+
+  //getters
+    public String GetMake() {
+    
+  }
+
+    public String GetModel() {
+    
+  }
+
+    public String GetColor() {
+    
+  }
+
+    public int GetYear() {
+    
+  }
+
+    public int GetMPG() {
+    
+  }
+
+    public int GetMake() {
+    
+  }
+
   
-}
+  //setters
+
+    public String SetMake() {
   
+  }
+  
+    public String SetModel() {
+  
+  }
+
+    public String SetColor() {
+  
+  }
+
+    public int SetYear() {
+  
+  }
+
+    public int SetMPG() {
+  
+  }
+
+    public int SetTopSpeed() {
+  
+  }
+
+  //toString
+  public String toString() {
+    return 
+  }
+
+  //equals
+  public boolean equals(Car otherCar){
+    if (this.
+  }
 }
